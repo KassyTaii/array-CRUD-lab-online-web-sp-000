5ff3cc38c7274dd8e344b0598e45cf030ee9c701
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-03-07 03:59:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,6 @@ def remove_element_from_end_of_array(array)
   [].pop
 end
 
-def remove_element_from_start_of_array(array)
-  [].shift
+def add_element_to_start_of_array(array, element)
+  [].unshift("element")
 end
